b3699b66c6338ea74b81266f5c8e9654681e35a6
Ziki <raynoldripper@gmail.com>
2024-02-15 16:29:10 +0300

    attestation tests correcting

---
+++ public/assets/AdminPage.1eacc83c.js
@@ -0,0 +1,1 @@
+import{_ as n,ab as o,J as s,O as a,K as i}from"./index.0043074d.js";const c={setup(){return{}},computed:{...o("user",["isAdmin"]),isAdminPage(){let e=!1;return this.$route.matched.map(function(t){t.name=="admin"&&(e=!0)}),e}},methods:{}};function m(e,t,p,u,d,_){const r=s("router-view");return i(),a(r)}var l=n(c,[["render",m]]);export{l as default};
