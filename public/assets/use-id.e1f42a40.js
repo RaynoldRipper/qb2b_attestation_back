74d7cd0a754e8c37dd74ca668c4d24eb8be1cbbe
Ziki <raynoldripper@gmail.com>
2024-03-20 16:35:47 +0300

    add black squade

--- public/assets/use-id.e1f42a40.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./uid.42677368.js";import{k as n,r as t,o as s}from"./index.0043074d.js";function f(r,u){return r===void 0?u===!0?`f_${e()}`:void 0:r}function d(r,u=!0){if(n.value===!0){const o=t(r);return u===!0&&r===void 0&&s(()=>{o.value=`f_${e()}`}),o}return t(f(r,u))}export{f as g,d as u};
