1cf570046a8d9abb91ea050190ed7dc467145500
Ziki <raynoldripper@gmail.com>
2024-11-06 11:33:40 +0300

    sync

--- public/assets/use-tick.c14e1809.js
+++
@@ -1,1 +0,0 @@
-import{a6 as i,f as s,F as a,a7 as r,g as c}from"./index.fa0b2748.js";function f(){let e;const o=c();function t(){e=void 0}return i(t),s(t),{removeTick:t,registerTick(n){e=n,a(()=>{e===n&&(r(o)===!1&&e(),e=void 0)})}}}export{f as u};
